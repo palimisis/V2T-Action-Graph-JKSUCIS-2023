fb07bc8dcfa0abc68e96669bfc1f7a8a9d13a772
Panagiotis Alimisis <panagiotis>
2024-08-09 02:07:56 +0300

    spatiotemp

---
+++ scripts/action_spatio_temporal_graph_feature_extractor.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --partition=luke
+#SBATCH --ntasks 1
+#SBATCH --gres=gpu:1
+#SBATCH --time=03:00:00
+
+
+# Activate Anaconda work environment
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate action_graph_env
+
+python feature_extractor/action_spatio_temporal_graph_feature_extractor.py
